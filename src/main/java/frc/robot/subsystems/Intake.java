68db6447ec6940934df94c50cf372fd5af38cbd0
Owen McCormick <41201059+owen-mccormick@users.noreply.github.com>
2024-01-26 17:43:15 -0800

    Vendor libraries, intake start, and checkstyle/linter

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Represents the intake subsystem. */
+public class Intake extends SubsystemBase {
+
+  /** Finite state machine options for the intake. */
+  public enum State {
+    UP,
+    DOWN,
+    OPENING,
+    CLOSING
+  }
+  
+  private static State state;
+  // TODO - set CAN IDs
+  private static final CANSparkMax pivot = new CANSparkMax(0, MotorType.kBrushless);
+  private static final CANSparkMax roller = new CANSparkMax(0, MotorType.kBrushless);
+
+  /** Constructs a new Intake object. */
+  public Intake() {
+    pivot.getPIDController().setP(0.004);
+    pivot.getPIDController().setI(0);
+    pivot.getPIDController().setD(0);
+  }
+
+  public State getState() {
+    return state;
+  }
+
+  public void setState(State state) {
+    Intake.state = state;
+  }
+
+  @Override
+  public void periodic() {
+    switch (state) {
+      case UP:
+        break;
+      case DOWN:
+        break;
+      case OPENING:
+        break;
+      case CLOSING:
+        break;
+      default:
+        break;
+    }
+  }
+}
